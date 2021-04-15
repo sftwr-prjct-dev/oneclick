a5bc763a0897ca5cbfc294576f44ebc03f301071
eltneg <yorewole@gmail.com>
2021-04-15 13:26:16 +0100

    Improve ui/ux

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,10 @@ import { useEffect, useState } from "react"
 import Head from 'next/head'
 import OneClick from '../scripts/ethereum'
 import { ToastContainer, toast } from 'react-toastify'
-const GSN = require('@opengsn/gsn')
 import 'react-toastify/dist/ReactToastify.css'
 import styles from '../styles/Home.module.css'
 import ClicksTable from "../components/ClicksTable"
 
-
-// const paymasterAddress = '0xfeF1fFc4aa52f50AbFf04316253e8e127c08b39F' // deployed paymaster address
-// const oneClickContractAddress = "0x716FFcB70043A48FBc049D51044d298D465612B9"
-// const nodeURL = "https://api.s0.b.hmny.io"
-
-// const paymasterAddress = '0x944Eef4A4D35502363CBEBc904f145B9C58909e2' // deployed paymaster address
-// const oneClickContractAddress = "0x04CCe397176F2Dd4075a12Fe158f0135868aaFFb"
-// const nodeURL = "https://api.s0.t.hmny.io"
 
 export default function Home() {
     const [connected, setConnected] = useState(false)
@@ -60,5 +51,7 @@ export default function Home() {
         </div>
     )
 }
+
+const formatAddress = (address: string) => `${address.slice(0,7)}...${address.slice(address.length-7,address.length)}`
 
 
